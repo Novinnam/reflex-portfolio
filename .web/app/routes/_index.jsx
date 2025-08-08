b220f355a8fb65a01ec4d955f6d45178fecf6e6b
Novinnam <024121135F@uni.lu>
2025-08-08 19:30:29 +0200

    the image of bell_bracket has been changed to 1024*1024 not it is square

--- .web/app/routes/_index.jsx
+++ .web/app/routes/_index.jsx
@@ -10,7 +10,7 @@ import { jsx } from "@emotion/react"
 
 
 
-function Text_17037081057075227923768297436447827437 () {
+function Text_99633831357110369685866192190511901652 () {
   
   const reflex___state____state__my_resume___my_resume____hero_state = useContext(StateContexts.reflex___state____state__my_resume___my_resume____hero_state)
 
@@ -22,8 +22,8 @@ function Text_17037081057075227923768297436447827437 () {
   return (
     jsx(
 RadixThemesText,
-{as:"p",css:({ ["@media screen and (min-width: 0px)"] : ({ ["fontSize"] : "20px", ["textAlign"] : "center" }), ["@media screen and (min-width: 48em)"] : ({ ["fontSize"] : "28px" }), ["@media screen and (min-width: 80em)"] : ({ ["fontSize"] : "50px" }), ["fontWeight"] : "medium", ["fontFamily"] : "Georgia, serif", ["--default-font-family"] : "Georgia, serif", ["lineHeight"] : "1.5", ["@media screen and (min-width: 62em)"] : ({ ["textAlign"] : "left" }) })},
-reflex___state____state__my_resume___my_resume____hero_state.bio_rx_state_
+{as:"p",css:({ ["fontSize"] : "18px", ["color"] : "#333333", ["lineHeight"] : "1.8", ["marginBottom"] : "1.5em", ["maxWidth"] : "1280px", ["@media screen and (min-width: 0px)"] : ({ ["textAlign"] : "center" }), ["@media screen and (min-width: 62em)"] : ({ ["textAlign"] : "left" }) })},
+reflex___state____state__my_resume___my_resume____hero_state.description_rx_state_
 ,)
   )
 }
@@ -43,7 +43,25 @@ function Img_313426746455881500796697668050988396767 () {
   )
 }
 
-function Grid_336718355531648194875752215371536324549 () {
+function Text_101293345110516020862261840462185915770 () {
+  
+  const reflex___state____state__my_resume___components____footer_state = useContext(StateContexts.reflex___state____state__my_resume___components____footer_state)
+
+
+
+
+
+  
+  return (
+    jsx(
+RadixThemesText,
+{as:"p",css:({ ["fontFamily"] : "monospace", ["--default-font-family"] : "monospace", ["fontSize"] : "0.8em", ["color"] : "white" })},
+("Version "+reflex___state____state__my_resume___components____footer_state.version_rx_state_+" \u2013 Date: "+reflex___state____state__my_resume___components____footer_state.today_rx_state_)
+,)
+  )
+}
+
+function Grid_120864026362213847007556087983552471422 () {
   
 
 
@@ -55,9 +73,9 @@ function Grid_336718355531648194875752215371536324549 () {
     jsx(
 RadixThemesGrid,
 {columns:({ ["base"] : "1", ["md"] : "1", ["lg"] : "2" }),gap:"4"},
-[({ ["title"] : "Rod Cap", ["description"] : "A compliance and buying SAAS for veterinary groups and their practices that I\u2019ve helped co-found.", ["image_url"] : "/rod_cap_perspective.png", ["border"] : true }), ({ ["title"] : "Bell Bracket", ["description"] : "A construction management SAAS for pharmaceutical turnover & commissioning that I\u2019ve helped co-found.", ["image_url"] : "/bell_bracket_perspective.png", ["border"] : true }), ({ ["title"] : "Single Power Screw", ["description"] : "A construction management SAAS for pharmaceutical turnover & commissioning that I\u2019ve helped co-found.", ["image_url"] : "/single_power_screw_perspective.png", ["border"] : true }), ({ ["title"] : "Coming Soon", ["description"] : "Coming soon", ["image_url"] : "/coming_soon.jpg", ["border"] : true })].map((project_rx_state_,index_237aef300e5197ff)=>(jsx(
+[({ ["title"] : "Rod Cap", ["description"] : "A compliance and buying SAAS for veterinary groups and their practices that I\u2019ve helped co-found.", ["image_url"] : "/rod_cap_perspective.png", ["border"] : true }), ({ ["title"] : "Bell Bracket", ["description"] : "A construction management SAAS for pharmaceutical turnover & commissioning that I\u2019ve helped co-found.", ["image_url"] : "/bell_bracket_perspective.png", ["border"] : true }), ({ ["title"] : "Single Power Screw", ["description"] : "A construction management SAAS for pharmaceutical turnover & commissioning that I\u2019ve helped co-found.", ["image_url"] : "/single_power_screw_perspective.png", ["border"] : true }), ({ ["title"] : "Coming Soon", ["description"] : "Coming soon", ["image_url"] : "/coming_soon.jpg", ["border"] : true })].map((project_rx_state_,index_f35afd34b31f7b3b)=>(jsx(
 RadixThemesBox,
-{css:({ ["padding_x"] : ({ ["base"] : "1em", ["md"] : "2em" }) }),key:index_237aef300e5197ff},
+{css:({ ["padding_x"] : ({ ["base"] : "1em", ["md"] : "2em" }) }),key:index_f35afd34b31f7b3b},
 jsx(
 RadixThemesFlex,
 {align:"start",className:"rx-Stack",direction:"column",gap:"3"},
@@ -74,7 +92,25 @@ project_rx_state_["description"]
   )
 }
 
-function Flex_295128056631617876652775366219327055982 () {
+function Text_17037081057075227923768297436447827437 () {
+  
+  const reflex___state____state__my_resume___my_resume____hero_state = useContext(StateContexts.reflex___state____state__my_resume___my_resume____hero_state)
+
+
+
+
+
+  
+  return (
+    jsx(
+RadixThemesText,
+{as:"p",css:({ ["@media screen and (min-width: 0px)"] : ({ ["fontSize"] : "20px", ["textAlign"] : "center" }), ["@media screen and (min-width: 48em)"] : ({ ["fontSize"] : "28px" }), ["@media screen and (min-width: 80em)"] : ({ ["fontSize"] : "50px" }), ["fontWeight"] : "medium", ["fontFamily"] : "Georgia, serif", ["--default-font-family"] : "Georgia, serif", ["lineHeight"] : "1.5", ["@media screen and (min-width: 62em)"] : ({ ["textAlign"] : "left" }) })},
+reflex___state____state__my_resume___my_resume____hero_state.bio_rx_state_
+,)
+  )
+}
+
+function Flex_110474036917116784462080327814605201104 () {
   
   const reflex___state____state__my_resume___my_resume____hero_state = useContext(StateContexts.reflex___state____state__my_resume___my_resume____hero_state)
 
@@ -87,50 +123,14 @@ function Flex_295128056631617876652775366219327055982 () {
     jsx(
 RadixThemesFlex,
 {align:"start",className:"rx-Stack",direction:"row",justify:({ ["initial"] : "center", ["md"] : "start" }),gap:"4"},
-reflex___state____state__my_resume___my_resume____hero_state.social_links_rx_state_.map((link_rx_state_,index_8729bb5b6f8565cd)=>(jsx(
+reflex___state____state__my_resume___my_resume____hero_state.social_links_rx_state_.map((link_rx_state_,index_2cf14dff640ba232)=>(jsx(
 RadixThemesLink,
-{asChild:true,css:({ ["color"] : "black", ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) }),key:index_8729bb5b6f8565cd},
+{asChild:true,css:({ ["color"] : "black", ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) }),key:index_2cf14dff640ba232},
 jsx(
 ReactRouterLink,
 {to:link_rx_state_["url"]},
 jsx(DynamicIcon,{name:link_rx_state_["platform"].replaceAll("_", "-")},)
 ,),))),)
-  )
-}
-
-function Text_99633831357110369685866192190511901652 () {
-  
-  const reflex___state____state__my_resume___my_resume____hero_state = useContext(StateContexts.reflex___state____state__my_resume___my_resume____hero_state)
-
-
-
-
-
-  
-  return (
-    jsx(
-RadixThemesText,
-{as:"p",css:({ ["fontSize"] : "18px", ["color"] : "#333333", ["lineHeight"] : "1.8", ["marginBottom"] : "1.5em", ["maxWidth"] : "1280px", ["@media screen and (min-width: 0px)"] : ({ ["textAlign"] : "center" }), ["@media screen and (min-width: 62em)"] : ({ ["textAlign"] : "left" }) })},
-reflex___state____state__my_resume___my_resume____hero_state.description_rx_state_
-,)
-  )
-}
-
-function Text_101293345110516020862261840462185915770 () {
-  
-  const reflex___state____state__my_resume___components____footer_state = useContext(StateContexts.reflex___state____state__my_resume___components____footer_state)
-
-
-
-
-
-  
-  return (
-    jsx(
-RadixThemesText,
-{as:"p",css:({ ["fontFamily"] : "monospace", ["--default-font-family"] : "monospace", ["fontSize"] : "0.8em", ["color"] : "white" })},
-("Version "+reflex___state____state__my_resume___components____footer_state.version_rx_state_+" \u2013 Date: "+reflex___state____state__my_resume___components____footer_state.today_rx_state_)
-,)
   )
 }
 
@@ -204,7 +204,7 @@ RadixThemesFlex,
 {align:"start",className:"rx-Stack",css:({ ["@media screen and (min-width: 0px)"] : ({ ["alignItems"] : "center" }), ["@media screen and (min-width: 62em)"] : ({ ["alignItems"] : "start" }) }),direction:"column",gap:"4"},
 jsx(Text_17037081057075227923768297436447827437,{},)
 ,jsx(Text_99633831357110369685866192190511901652,{},)
-,jsx(Flex_295128056631617876652775366219327055982,{},)
+,jsx(Flex_110474036917116784462080327814605201104,{},)
 ,),),),jsx(
 RadixThemesBox,
 {css:({ ["backgroundColor"] : "black", ["padding_x"] : ({ ["base"] : "1em", ["md"] : "2em" }), ["width"] : "100%", ["maxWidth"] : "1344px", ["marginInlineStart"] : "auto", ["marginInlineEnd"] : "auto", ["height"] : "42px" })},
@@ -229,7 +229,7 @@ jsx(
 RadixThemesHeading,
 {css:({ ["marginBottom"] : "1em", ["fontFamily"] : "Georgia, serif", ["--default-font-family"] : "Georgia, serif", ["font_size"] : ({ ["base"] : "32px", ["md"] : "48px", ["lg"] : "60px" }), ["textAlign"] : "center" })},
 "Featured Projects"
-,),jsx(Grid_336718355531648194875752215371536324549,{},)
+,),jsx(Grid_120864026362213847007556087983552471422,{},)
 ,jsx(
 RadixThemesLink,
 {asChild:true,css:({ ["fontWeight"] : "bold", ["color"] : "red", ["marginTop"] : "2em", ["alignSelf"] : "flex-end", ["&:hover"] : ({ ["color"] : "var(--accent-8)" }) })},
