d6425c8160a5930c8367c01c6815952d071c27a0
Novinnam <024121135F@uni.lu>
2025-08-11 20:26:20 +0200

    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'] is added to rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="my_resume", # Ensure this matches your file name
+    app_name="my_resume",  # Ensure this matches your file name
+    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
